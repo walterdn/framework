54a774d843913ca93a1ddfb1f7f46e8718363321
Jenny Pollack <JennyPollack@Jennys-MacBook-Pro.local>
2015-11-09 22:00:37 -0800

    add chaiHttp

--- test/test.js
+++ test/test.js
@@ -1,15 +1,19 @@
 var chai = require('chai');
 var expect = chai.expect;
 var chaiHttp = require('chai-http');
-var t = require(__dirname + '/../lib/troutr.js');
+var Troutr = require(__dirname + '/../lib/troutr.js');
+var troutr = new Troutr();
+
+chai.use(chaiHttp);
 
 describe('The troutr npm package', function(){
   it('should create a path named /troutr', function(){
-    chai.request(app)
+    chai.request('localhost:3000')
       .get('/')
       .end(function(err, res){
         expect(err).to.be.null;
         expect(res).to.have.status(200);
+
       });
   });
 });
